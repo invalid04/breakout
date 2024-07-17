8f9b2cb04be11f61b70dd7bfc8a0b72975de2395
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-17 03:28:17 -0500

    added elements

--- breakout/index.js
+++ breakout/index.js
@@ -14,7 +14,21 @@ class Block {
 
 // all blocks
 const blocks = [
-    new Block(10, 270)
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    // new Block(560, 270),
+    // new Block(670, 270),
+    // new Block(780, 270),
+    // new Block(890, 270),
+    // new Block(1000, 270),
+    // new Block(1110, 270),
+    // new Block(1220, 270),
+    // new Block(1330, 270),
+    // new Block(1440, 270),
+    // new Block(1550, 270),
 ]
 
 // draw blocks
