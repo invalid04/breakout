47cfcd3f23657e6017942f9953a22c8122227c4c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-17 03:15:52 -0500

    created addBlock function

--- breakout/index.js
+++ breakout/index.js
@@ -1,5 +1,10 @@
 const grid = document.querySelector('.grid')
 
-const block = document.createElement('div')
-block.classList.add('block')
-grid.appendChild(block)
+// draw block
+function addBlock() {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = '100px'
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+}
