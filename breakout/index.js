92bad0df51d15a6033e095bee1c836b2a14ddc05
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-17 03:41:08 -0500

    fixed bug in moving left

--- breakout/index.js
+++ breakout/index.js
@@ -65,7 +65,13 @@ function drawUser() {
 function moveUser(e) {
     switch(e.key) {
         case 'ArrowLeft':
-            currentPosition[0] -= 10
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10
+                drawUser()
+            }
+            break
+        case 'ArrowRight':
+            currentPosition[0] += 10
             drawUser()
             break
     }
