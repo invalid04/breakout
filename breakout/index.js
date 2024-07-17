7169de0603cc32b91c198ff053ba36ef01dc94e1
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-17 03:19:43 -0500

    created block js class

--- breakout/index.js
+++ breakout/index.js
@@ -1,4 +1,16 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+
+// create block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
 
 // draw block
 function addBlock() {
@@ -8,3 +20,5 @@ function addBlock() {
     block.style.bottom = '50px'
     grid.appendChild(block)
 }
+
+addBlock()
