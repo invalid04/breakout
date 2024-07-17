0cf637142297ea8cb69b8f1846f87deb4a3c39f3
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-17 04:19:01 -0500

    collision with blocks

--- breakout/index.js
+++ breakout/index.js
@@ -6,7 +6,7 @@ const ballDiameter = 20
 const boardWidth = 560
 const boardHeight = 300
 let timerId
-let xDirection = 2
+let xDirection = -2
 let yDirection = 2
 
 const userStart = [230, 10]
@@ -134,6 +134,18 @@ function changeDirection() {
 
 // check for collisions
 function checkForCollisions() {
+    // check for block collisions
+    for (let i = 0; i < blocks.length; i++) {
+        if (
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ) {
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            allBlocks[i].classList.remove('block')
+            blocks.splice(i, 1)
+        }
+    }
+
     // check for wall collisions
     if (
         ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
@@ -147,5 +159,6 @@ function checkForCollisions() {
     if (ballCurrentPosition[1] <= 0) {
         clearInterval(timerId)
         scoreDisplay.innerHTML = 'you lose'
+        document.removeEventListener('keydown', moveUser)
     }
 }
