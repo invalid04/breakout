163abfb59a3aff9880062eb5e61dbd5a76dd6751
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-17 04:09:46 -0500

    created score element

--- breakout/index.js
+++ breakout/index.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
 const ballDiameter = 20
@@ -145,5 +146,6 @@ function checkForCollisions() {
     // check for game over 
     if (ballCurrentPosition[1] <= 0) {
         clearInterval(timerId)
+        scoreDisplay.innerHTML = 'you lose'
     }
 }
